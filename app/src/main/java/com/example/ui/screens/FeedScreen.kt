f7de4b7a3f553f400be7f599a7166b19a013e826
bsss87345-cell <bsss87345@gmail.com>
2026-09-16 01:57:49 +0300

    Update FeedScreen.kt

--- app/src/main/java/com/example/ui/screens/FeedScreen.kt
+++ app/src/main/java/com/example/ui/screens/FeedScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material.icons.outlined.*
 import androidx.compose.material3.*
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Alignment
