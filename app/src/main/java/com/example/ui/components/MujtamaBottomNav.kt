459ecfbafd172974fe7b4e566272c3e87d511fb7
bsss87345-cell <bsss87345@gmail.com>
2026-09-18 12:07:38 +0300

    Update MujtamaBottomNav.kt

--- app/src/main/java/com/example/ui/components/MujtamaBottomNav.kt
+++ app/src/main/java/com/example/ui/components/MujtamaBottomNav.kt
@@ -1,5 +1,6 @@
 package com.example.ui.components
 
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
