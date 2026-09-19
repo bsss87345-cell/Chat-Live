a8590bbdf64f9f3be7d90e8f47fb8fbdc1c104c3
bsss87345-cell <bsss87345@gmail.com>
2026-09-19 05:07:18 +0300

    Update FeedScreen.kt

--- app/src/main/java/com/example/ui/screens/FeedScreen.kt
+++ app/src/main/java/com/example/ui/screens/FeedScreen.kt
@@ -40,6 +40,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
