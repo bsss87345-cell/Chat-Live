0f3d7f8c9e6ab577fc4e3e286188b8d321d1994e
bsss87345-cell <bsss87345@gmail.com>
2026-09-19 04:33:52 +0300

    Update AccountSettingsScreen.kt

--- app/src/main/java/com/example/ui/screens/AccountSettingsScreen.kt
+++ app/src/main/java/com/example/ui/screens/AccountSettingsScreen.kt
@@ -1,6 +1,8 @@
 package com.example.ui.screens
 
+import android.graphics.BitmapFactory
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
