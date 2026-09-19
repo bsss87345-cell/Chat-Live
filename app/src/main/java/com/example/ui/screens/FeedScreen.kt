a29fc7cde16e3248f349c2afa36552e54c487f28
bsss87345-cell <bsss87345@gmail.com>
2026-09-19 05:05:27 +0300

    Update FeedScreen.kt

--- app/src/main/java/com/example/ui/screens/FeedScreen.kt
+++ app/src/main/java/com/example/ui/screens/FeedScreen.kt
@@ -3,6 +3,7 @@ package com.example.ui.screens
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import android.view.WindowManager
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.window.DialogWindowProvider
