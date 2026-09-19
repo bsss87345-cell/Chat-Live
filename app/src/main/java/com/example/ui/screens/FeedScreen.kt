8f850b64e516b1f61e44cb7d303efbd6670ad59d
bsss87345-cell <bsss87345@gmail.com>
2026-09-19 16:00:46 +0300

    Update FeedScreen.kt

--- app/src/main/java/com/example/ui/screens/FeedScreen.kt
+++ app/src/main/java/com/example/ui/screens/FeedScreen.kt
@@ -13,6 +13,7 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.Camera
+import androidx.camera.core.ImageCapture
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
