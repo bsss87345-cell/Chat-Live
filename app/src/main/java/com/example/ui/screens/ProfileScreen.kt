3cbe239ff3c13a9e9689d5b5544131aabb90f421
bsss87345-cell <bsss87345@gmail.com>
2026-09-25 12:41:39 +0300

    Update ProfileScreen.kt

--- app/src/main/java/com/example/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/ui/screens/ProfileScreen.kt
@@ -59,6 +59,8 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.ui.draw.shadow
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
