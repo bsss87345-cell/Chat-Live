460861ca0986b55232dc21e205d29cff078638a5
bsss87345-cell <bsss87345@gmail.com>
2026-09-26 03:21:12 +0300

    Update ChatScreen.kt

--- app/src/main/java/com/example/ui/screens/ChatScreen.kt
+++ app/src/main/java/com/example/ui/screens/ChatScreen.kt
@@ -63,6 +63,7 @@ import android.net.Uri
 import coil.compose.AsyncImage
 import androidx.compose.ui.res.painterResource
 import com.example.R
+import com.aistudio.mujtamauna.ar8vzp.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
